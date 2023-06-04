9d3761dbd7a743754ae91ba9551a43ad23ef54cd
Ivan Divanich <56882376+IvanDivanich@users.noreply.github.com>
2023-06-04 22:48:22 +0300

    Add files via upload

---
+++ Pr_11/Пр11/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Пр11
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap[] Frames;
+        private int FrameNum;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Frames = new Bitmap[18];
+
+            for (int i = 0; i < 18; i++)
+            {
+                Frames[i] = new Bitmap("Frame" + i + ".png");
+            }
+
+            pictureBox1.Image = Frames[FrameNum];
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (trackBar1.Value == 0)
+            {
+                timer1.Interval = 500;
+                FrameNum = ++FrameNum % Frames.Length;
+                pictureBox1.Image = Frames[FrameNum];
+            }
+            else if (trackBar1.Value == 1)
+            {
+                timer1.Interval = 450;
+                FrameNum = ++FrameNum % Frames.Length;
+                pictureBox1.Image = Frames[FrameNum];
+            }
+            else if (trackBar1.Value == 2)
+            {
+                timer1.Interval = 400;
+                FrameNum = ++FrameNum % Frames.Length;
+                pictureBox1.Image = Frames[FrameNum];
+            }
+            else if (trackBar1.Value == 3)
+            {
+                timer1.Interval = 350;
+                FrameNum = ++FrameNum % Frames.Length;
+                pictureBox1.Image = Frames[FrameNum];
+            }
+            else if (trackBar1.Value == 4)
+            {
+                timer1.Interval = 300;
+                FrameNum = ++FrameNum % Frames.Length;
+                pictureBox1.Image = Frames[FrameNum];
+            }
+            else if (trackBar1.Value == 5)
+            {
+                timer1.Interval = 250;
+                FrameNum = ++FrameNum % Frames.Length;
+                pictureBox1.Image = Frames[FrameNum];
+            }
+            else if (trackBar1.Value == 6)
+            {
+                timer1.Interval = 200;
+                FrameNum = ++FrameNum % Frames.Length;
+                pictureBox1.Image = Frames[FrameNum];
+            }
+            else if (trackBar1.Value == 7)
+            {
+                timer1.Interval = 150;
+                FrameNum = ++FrameNum % Frames.Length;
+                pictureBox1.Image = Frames[FrameNum];
+            }
+            else if (trackBar1.Value == 8)
+            {
+                timer1.Interval = 100;
+                FrameNum = ++FrameNum % Frames.Length;
+                pictureBox1.Image = Frames[FrameNum];
+            }
+            else if (trackBar1.Value == 9)
+            {
+                timer1.Interval = 50;
+                FrameNum = ++FrameNum % Frames.Length;
+                pictureBox1.Image = Frames[FrameNum];
+            }
+            else if (trackBar1.Value == 10)
+            {
+                timer1.Interval = 5;
+                FrameNum = ++FrameNum % Frames.Length;
+                pictureBox1.Image = Frames[FrameNum];
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+            if (timer1.Enabled)
+            {
+                button1.Text = "Стоп";
+            }
+            else
+            {
+                button1.Text = "Старт";
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Pr_11/Пр11/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Пр11")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Пр11")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("24caf299-5245-478d-af5c-46bac8193348")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
